8731962cbf58301cd03965a0a12a35662b3b7c04
Josh Long <josh@joshlong.com>
2020-05-28 18:57:00 -0700

    closes #14

--- src/main/kotlin/ttd/site/generator/Step0Configuration.kt
+++ src/main/kotlin/ttd/site/generator/Step0Configuration.kt
@@ -15,41 +15,44 @@ import java.util.*
 
 @Configuration
 class Step0Configuration(
-		private val siteGenerationJobState: SiteGenerationJobState,
-		private val templateService: TemplateService,
-		private val jdbcTemplate: JdbcTemplate,
-		private val siteGenerationProperties: SiteGeneratorConfigurationProperties,
-		private val sbf: StepBuilderFactory) {
+    private val siteGenerationJobState: SiteGenerationJobState,
+    private val templateService: TemplateService,
+    private val jdbcTemplate: JdbcTemplate,
+    private val siteGenerationProperties: SiteGeneratorConfigurationProperties,
+    private val sbf: StepBuilderFactory) {
 
-	companion object {
-		const val STEP_NAME = "step0"
-	}
+  companion object {
+    const val STEP_NAME = "step0"
+  }
 
-	@Bean(STEP_NAME)
-	fun step(): Step {
-		return sbf //
-				.get(STEP_NAME) //
-				.tasklet { _: StepContribution, _: ChunkContext ->
-					val newSql =
-							"""
+  @Bean(STEP_NAME)
+  fun step(): Step {
+    return sbf //
+        .get(STEP_NAME) //
+        .tasklet { _: StepContribution, _: ChunkContext ->
+          val newSql =
+              """
 								select distinct
 									date_part('year', time) || '-' || lpad(date_part('month', time)||'', 2, '0') as year_and_month,
 									date_part('year', time) as year,
 									date_part('month', time) as month
 								from bookmark b
-								where b.deleted = false
+								where
+								 b.deleted = false 
+								and 
+									b.edited is not null 
 							"""
-					val yearAndMonths = this.jdbcTemplate.query(newSql) { rs, _ -> YearMonth(rs.getInt("year"), rs.getInt("month")) }
-					yearAndMonths.sortWith(Comparator.naturalOrder())
-					val html = templateService.years(yearAndMonths)
-					val file = File(siteGenerationProperties.contentDirectory.file, "years.include")
-					BufferedWriter(FileWriter(file)).use {
-						it.write(html)
-					}
-					val yearMonth = yearAndMonths[0]
-					siteGenerationJobState.latestYearMonth.set(yearMonth)
-					RepeatStatus.FINISHED
-				} //
-				.build()
-	}
+          val yearAndMonths = this.jdbcTemplate.query(newSql) { rs, _ -> YearMonth(rs.getInt("year"), rs.getInt("month")) }
+          yearAndMonths.sortWith(Comparator.naturalOrder())
+          val html = templateService.years(yearAndMonths)
+          val file = File(siteGenerationProperties.contentDirectory.file, "years.include")
+          BufferedWriter(FileWriter(file)).use {
+            it.write(html)
+          }
+          val yearMonth = yearAndMonths[0]
+          siteGenerationJobState.latestYearMonth.set(yearMonth)
+          RepeatStatus.FINISHED
+        } //
+        .build()
+  }
 }
