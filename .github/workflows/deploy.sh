a3d50e9df9a3e10d835b368ee4549f45181ebc0b
Josh Long <josh@joshlong.com>
2021-01-25 04:53:12 -0800

    deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,21 +5,21 @@ APP_NAME=site-generator
 PROJECT_ID=${GCLOUD_PROJECT}
 
 ## Create the Docker Image
-TAG_NAME=${1:-$(date +%s)}
-IMAGE_TAG="production${GITHUB_SHA:-}"
-GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/twi-ttd-${APP_NAME}
-mvn -f ${ROOT_DIR}/../../pom.xml -DskipTests=true \
-  -e -Dspring.profiles.active=production  \
-  clean \
-  verify \
-  deploy \
-  spring-boot:build-image
-
-image_id=$(docker images -q $APP_NAME)
-docker tag "${image_id}" ${GCR_IMAGE_NAME}:latest
-docker tag "${image_id}" ${GCR_IMAGE_NAME}:${IMAGE_TAG}
-docker push ${GCR_IMAGE_NAME}:latest
-docker push ${GCR_IMAGE_NAME}:${IMAGE_TAG}
+#TAG_NAME=${1:-$(date +%s)}
+#IMAGE_TAG="production${GITHUB_SHA:-}"
+#GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/twi-ttd-${APP_NAME}
+#mvn -f ${ROOT_DIR}/../../pom.xml -DskipTests=true \
+#  -e -Dspring.profiles.active=production  \
+#  clean \
+#  verify \
+#  deploy \
+#  spring-boot:build-image
+#
+#image_id=$(docker images -q $APP_NAME)
+#docker tag "${image_id}" ${GCR_IMAGE_NAME}:latest
+#docker tag "${image_id}" ${GCR_IMAGE_NAME}:${IMAGE_TAG}
+#docker push ${GCR_IMAGE_NAME}:latest
+#docker push ${GCR_IMAGE_NAME}:${IMAGE_TAG}
 
 ## Deploy the application to Kubernetes
 cd $GITHUB_WORKSPACE/deploy
@@ -28,9 +28,9 @@ touch $SECRETS_FN
 echo writing to "${SECRETS_FN}..."
 cat <<EOF >${SECRETS_FN}
 apiVersion: v1
-kind: Secret
+kind: ConfigMap
 metadata:
-  name: twi-ttd-secrets
+  name: twi-ttd-site-generator-config
 type: Opaque
 data:
   SPRING_PROFILES_ACTIVE: "cloud"
@@ -41,8 +41,7 @@ data:
   GIT_USERNAME : "${GIT_PASSWORD}"
 EOF
 
-
 kubectl apply -f .
-rm $SECRETS_FN
-cd $GITHUB_WORKSPACE
-kubectl create job --from=cronjob/${NS}-twi-twitter-ingest-cronjob ${NS}-twi-twitter-ingest-cronjob-${RANDOM} -n $NS
+rm ${SECRETS_FN}
+cd ${GITHUB_WORKSPACE}
+#kubectl create job --from=cronjob/${NS}-twi-twitter-ingest-cronjob ${NS}-twi-twitter-ingest-cronjob-${RANDOM} -n $NS
