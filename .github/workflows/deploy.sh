9edbeeb6797445651683495995f55fdd6213c560
Josh Long <josh@joshlong.com>
2021-08-20 01:30:18 -0700

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,12 +8,12 @@ PROJECT_ID=${GCLOUD_PROJECT}
 TAG_NAME=${1:-$(date +%s)}
 IMAGE_TAG="production${GITHUB_SHA:-}"
 GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/twi-ttd-${APP_NAME}
-mvn -f ${ROOT_DIR}/../../pom.xml -DskipTests=true \
-  -e -Dspring.profiles.active=production  \
-  clean \
-  verify \
-  deploy \
-  spring-boot:build-image
+#mvn -f ${ROOT_DIR}/../../pom.xml -DskipTests=true \
+#  -e -Dspring.profiles.active=production  \
+#  clean \
+#  verify \
+#  deploy \
+#  spring-boot:build-image
 
 image_id=$(docker images -q $APP_NAME)
 docker tag "${image_id}" ${GCR_IMAGE_NAME}:latest
